59bd5a2b3ce5c99fcaaa463bc4342f4d4d310954
omahm <hello@onemanandhismouse.com>
2019-09-02 09:47:41 +0100

    D8NID-291 ~ Added 2 additional layout examples

---
+++ nicsdru_layouts/src/Plugin/Layout/Gehry.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace Drupal\nicsdru_layouts\Plugin\Layout;
+
+use Drupal\nicsdru_layouts\NicsDruLayout;
+
+/**
+*
+* @Layout(
+*   id = "nicsdru_layouts_gehry",
+*   label = @Translation("Gehry"),
+*   category = @Translation("NICS Origins"),
+*   template = "templates/nicsdru-layouts--gehry",
+*   icon_map = {
+*     { "main", "sidebar" }
+*   },
+*   regions = {
+*     "main" = {
+*       "label" = @Translation("Main content"),
+*     },
+*      "sidebar" = {
+*       "label" = @Translation("Sidebar content"),
+*     }
+*   }
+* )
+*/
+class Gehry extends NicsDruLayout {
+
+}
