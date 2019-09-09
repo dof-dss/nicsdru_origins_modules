3bf0c82656068d65538f769c39df902dac685176
DuttonMa <martin@theduttons.org>
2019-09-09 16:30:41 +0100

    Use placeholders for logging

--- origins_workflow/src/Controller/ModerationStateController.php
+++ origins_workflow/src/Controller/ModerationStateController.php
@@ -60,8 +60,11 @@ class ModerationStateController extends ControllerBase implements ContainerInjec
       $entity->set('moderation_state', $new_state);
       $entity->save();
       // Log it.
-      $message = t('State of') . " '" . t($entity->getTitle()) . "' (nid " . $nid . ") " . t('changed to');
-      $message .= ' ' . $new_state . ' ' . t('by') . ' ' . $this->currentUser()->getAccountName();
+      $message = t('State of @title (nid @nid) changed to @new_state by @user',
+        ['@title' => $entity->getTitle(),
+          '@nid' => $nid,
+          '@new_state' => $new_state,
+          '@user' => $this->currentUser()->getAccountName()]);
       $this->logger->notice($message);
     }
     // Redirect user to current page (although the 'destination'
