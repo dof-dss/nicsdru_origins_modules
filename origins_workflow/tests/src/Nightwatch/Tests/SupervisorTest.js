d030b8d86ae5b96c2b216b811ede3c5f2c5dbf2b
DuttonMa <martin@theduttons.org>
2019-09-20 09:07:30 +0100

    working test with user set up in install

--- origins_workflow/tests/src/Nightwatch/Tests/SupervisorTest.js
+++ origins_workflow/tests/src/Nightwatch/Tests/SupervisorTest.js
@@ -1,45 +1,14 @@
 module.exports = {
-  '@tags': ['core'],
-
-  before(browser) {
-    browser.drupalInstall();
-  },
-  after(browser) {
-    browser.drupalUninstall();
-  },
+  '@tags': ['nidirect-migrations', 'nidirect-files'],
 
   'Test login': browser => {
     browser
-      .drupalCreateUser({
-        name: 'night_supervisor',
-        password: 'password',
-        permissions: ['access content'],
-      })
-      .drupalLogin({ name: 'night_supervisor', password: 'password' })
-      .drupalRelativeURL('/admin/reports')
+      .drupalLogin({ name: 'nw_test_supervisor', password: process.env.TEST_PASS });
+
+    browser
+      .drupalRelativeURL('/admin/workflow/drafts')
       .expect.element('h1.page-title')
-      .text.to.contain('Reports');
-  },
-};
-
-module.exports = {
-  '@tags': ['nidirect-migrations', 'nidirect-files'],
-
-  'Creates a supervisor': browser => {
-    browser
-      .drupalLogin({ name: process.env.TEST_USER, password: process.env.TEST_PASS });
-
-    browser
-      .drupalCreateUser({
-        name: 'night_supervisor',
-        password: 'password',
-        permissions: ['access content'],
-      });
-
-    browser.drupalLogout();
-
-    browser
-      .drupalLogin({ name: 'night_supervisor', password: 'password' });
-
+      .text.to.contain('My Drafts');
   }
+
 };
