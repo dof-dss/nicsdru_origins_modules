dd5cb27752b92cf2de6da44315a63db6d83fdb13
DuttonMa <martin@theduttons.org>
2019-10-07 12:04:54 +0100

    test corrections for users that should not see the dashboard

--- origins_workflow/tests/src/Nightwatch/Tests/AppsTest.js
+++ origins_workflow/tests/src/Nightwatch/Tests/AppsTest.js
@@ -8,12 +8,12 @@ module.exports = {
     browser
       .drupalRelativeURL('/admin/workflow/drafts')
       .expect.element('h1.page-title')
-      .text.to.contain('My Drafts');
+      .text.to.contain('Access denied');
 
     browser
       .drupalRelativeURL('/admin/workflow/needs-review')
       .expect.element('h1.page-title')
-      .text.to.contain('Needs Review');
+      .text.to.contain('Access denied');
   }
 
 };
