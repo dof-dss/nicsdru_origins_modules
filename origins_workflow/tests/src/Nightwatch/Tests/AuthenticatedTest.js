0d38d841e701afc1dd3f703f01ff300ee022c6cc
DuttonMa <martin@theduttons.org>
2019-10-07 12:19:14 +0100

    added more negative tests

--- origins_workflow/tests/src/Nightwatch/Tests/AuthenticatedTest.js
+++ origins_workflow/tests/src/Nightwatch/Tests/AuthenticatedTest.js
@@ -6,12 +6,27 @@ module.exports = {
       .drupalLogin({ name: 'nw_test_authenticated', password: process.env.TEST_PASS });
 
     browser
+      .drupalRelativeURL('/node/add')
+      .expect.element('h1.page-title')
+      .text.to.contain('Access denied');
+
+    browser
       .drupalRelativeURL('/admin/workflow/drafts')
       .expect.element('h1.page-title')
       .text.to.contain('Access denied');
 
     browser
+      .drupalRelativeURL('/admin/workflow/all-drafts')
+      .expect.element('h1.page-title')
+      .text.to.contain('Access denied');
+
+    browser
       .drupalRelativeURL('/admin/workflow/needs-review')
+      .expect.element('h1.page-title')
+      .text.to.contain('Access denied');
+
+    browser
+      .drupalRelativeURL('/admin/workflow/needs-audit')
       .expect.element('h1.page-title')
       .text.to.contain('Access denied');
   }
