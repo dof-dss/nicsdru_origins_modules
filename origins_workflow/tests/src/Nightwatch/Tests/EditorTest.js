e966ddc66c14cb87010ef815ed1926b05b07b38f
DuttonMa <martin@theduttons.org>
2019-10-28 14:51:30 +0000

    fixed 'site themes' test for all

--- origins_workflow/tests/src/Nightwatch/Tests/EditorTest.js
+++ origins_workflow/tests/src/Nightwatch/Tests/EditorTest.js
@@ -6,9 +6,9 @@ module.exports = {
       .drupalLogin({ name: process.env.NW_TEST_USER_PREFIX + '_editor', password: process.env.TEST_PASS });
 
     browser
-      .drupalRelativeURL('/admin/structure/taxonomy_manager/voc')
+      .drupalRelativeURL('/admin/site_themes')
       .expect.element('h1.page-title')
-      .text.to.contain('Taxonomy Manager');
+      .text.to.contain('Site Themes');
 
     browser
       .drupalRelativeURL('/node/add')
