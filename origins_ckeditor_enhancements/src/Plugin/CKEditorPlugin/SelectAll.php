3ee8eb6ee66b5abe39df7e50cb675572af51a001
DuttonMa <martin@theduttons.org>
2019-12-05 17:00:30 +0000

    port CKEditor enhancements over from Cyber Security

---
+++ origins_ckeditor_enhancements/src/Plugin/CKEditorPlugin/SelectAll.php
@@ -0,0 +1,84 @@
+<?php
+
+namespace Drupal\origins_ckeditor_enhancements\Plugin\CKEditorPlugin;
+
+use Drupal\ckeditor\CKEditorPluginBase;
+use Drupal\editor\Entity\Editor;
+
+/**
+ * Defines the "selectall" plugin.
+ *
+ * NOTE: The plugin ID ('id' key) corresponds to the CKEditor plugin name.
+ * It is the first argument of the CKEDITOR.plugins.add() function in the
+ * plugin.js file.
+ *
+ * @CKEditorPlugin(
+ *   id = "selectall",
+ *   label = @Translation("Select all"),
+ *   module = "origins_ckeditor_enhancements"
+ * )
+ */
+class SelectAll extends CKEditorPluginBase {
+
+  /**
+   * {@inheritdoc}
+   *
+   * NOTE: The keys of the returned array corresponds to the CKEditor button
+   * names. They are the first argument of the editor.ui.addButton() or
+   * editor.ui.addRichCombo() functions in the plugin.js file.
+   */
+  public function getButtons() {
+    // Make sure that the path to the image matches the file structure of
+    // the CKEditor plugin you are implementing.
+    return [
+      'SelectAll' => [
+        'label' => t('Select all'),
+        'image' => 'libraries/ckeditor/plugins/selectall/icons/selectall.png',
+      ],
+    ];
+  }
+
+  /**
+   * {@inheritdoc}
+   */
+  public function isEnabled(Editor $editor) {
+  }
+
+  /**
+   * {@inheritdoc}
+   */
+  public function getFile() {
+    // Make sure that the path to the plugin.js matches the file structure of
+    // the CKEditor plugin you are implementing.
+    return 'libraries/ckeditor/plugins/selectall/plugin.js';
+  }
+
+  /**
+   * {@inheritdoc}
+   */
+  public function isInternal() {
+    return FALSE;
+  }
+
+  /**
+   * {@inheritdoc}
+   */
+  public function getDependencies(Editor $editor) {
+    return [];
+  }
+
+  /**
+   * {@inheritdoc}
+   */
+  public function getLibraries(Editor $editor) {
+    return [];
+  }
+
+  /**
+   * {@inheritdoc}
+   */
+  public function getConfig(Editor $editor) {
+    return [];
+  }
+
+}
