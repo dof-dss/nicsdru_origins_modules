dd9dbe5399b60cb02e1976579960369f11bda3e9
Johan Gant <johan.gant@gmail.com>
2019-12-20 10:37:06 +0000

    Introduce extra permission check for anonymous users.
    - Ensures they can still view term canonical pages.
    - Ensures they cannot sneak in to view adapted taxonomy manager routes.

--- origins_taxonomy_access/src/TaxonomyVocabAccess.php
+++ origins_taxonomy_access/src/TaxonomyVocabAccess.php
@@ -16,6 +16,16 @@ class TaxonomyVocabAccess {
 
   /**
    * Access callback for common CUSTOM taxonomy operations.
+   *
+   * We generally DO want to allow anonymous users access to view term pages.
+   * We can't re-use that permission for taxonomy manager routes, because it would allow
+   * anonymous users the ability to view those routes/inspect the term hierarchy and potentially manipulate
+   * taxonomy data which would be a Bad Thing (TM).
+   *
+   * So we're predicating this approach by saying something like:
+   * - If you can administer all things taxonomy (aka: Lord Of The Terms), then grant access early.
+   * - If you can't view the admin pages (aka: Anonymous User), then reject early.
+   * - Otherwise, evaluate what you can/cannot do in more detail.
    */
   public static function handleAccess(Route $route, RouteMatchInterface $match) {
     $op = $route->getOption('op');
@@ -26,14 +36,19 @@ class TaxonomyVocabAccess {
       return AccessResult::allowed();
     }
     else {
+      // If the user can't view admin pages, reject the request early.
+      if (\Drupal::currentUser()->hasPermission('access administration pages') == FALSE) {
+        return AccessResult::forbidden();
+      }
+
       // Check user can view terms in this vocab.
       if ($match->getRouteName() == 'taxonomy_manager.admin_vocabulary') {
-        return AccessResult::allowedIfHasPermission(\Drupal::currentUser(), 'view terms in ' . $taxonomy_vocabulary->id());
+        return AccessResult::allowedIfHasPermission('view terms in ' . $taxonomy_vocabulary->id());
       }
 
       // Loosely map these outlying tasks to a general permission for the vocab.
       if (in_array($op, ['search', 'searchautocomplete'])) {
-        return AccessResult::allowedIfHasPermission(\Drupal::currentUser(), 'edit terms in ' . $taxonomy_vocabulary->id());
+        return AccessResult::allowedIfHasPermission('edit terms in ' . $taxonomy_vocabulary->id());
       }
 
       // Move == reorder; difference in semantics between taxonomy_manager and taxonomy_access_fix.
