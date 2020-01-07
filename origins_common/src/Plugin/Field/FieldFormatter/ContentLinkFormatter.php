7e57bbc149949717f3c12c731e7bffb0216c5d55
omahm <hello@onemanandhismouse.com>
2020-01-07 15:54:53 +0000

    D8O-14 ~ Trim field output

--- origins_common/src/Plugin/Field/FieldFormatter/ContentLinkFormatter.php
+++ origins_common/src/Plugin/Field/FieldFormatter/ContentLinkFormatter.php
@@ -6,6 +6,8 @@ use Drupal\Core\Field\FormatterBase;
 use Drupal\Core\Field\FieldItemListInterface;
 use Drupal\Core\Form\FormStateInterface;
 use Drupal\Core\Link;
+use Drupal\Core\Security\TrustedCallbackInterface;
+use Drupal\text\Plugin\Field\FieldFormatter\TextTrimmedFormatter;
 
 /**
  * Field formatter for displaying a link to the entity.
@@ -22,7 +24,7 @@ use Drupal\Core\Link;
  *   }
  * )
  */
-class ContentLinkFormatter extends FormatterBase {
+class ContentLinkFormatter extends FormatterBase implements TrustedCallbackInterface {
 
   public static function defaultSettings()
   {
@@ -85,19 +87,73 @@ class ContentLinkFormatter extends FormatterBase {
   public function viewElements(FieldItemListInterface $items, $langcode) {
     $elements = [];
 
+    $render_as_summary = function (&$element) {
+      // Make sure any default #pre_render callbacks are set on the element,
+      // because text_pre_render_summary() must run last.
+      $element += \Drupal::service('element_info')->getInfo($element['#type']);
+      // Add the #pre_render callback that renders the text into a summary.
+      $element['#pre_render'][] = [TextTrimmedFormatter::class, 'preRenderSummary'];
+      // Pass on the trim length to the #pre_render callback via a property.
+      $element['#text_summary_trim_length'] = $this->getSetting('trim_length');
+    };
+
+    // The ProcessedText element already handles cache context & tag bubbling.
+    // @see \Drupal\filter\Element\ProcessedText::preRenderText()
     foreach ($items as $delta => $item) {
       $elements[$delta] = [
         '#type' => 'processed_text',
-        '#text' => $item->value,
+        '#text' => NULL,
         '#format' => $item->format,
         '#langcode' => $item->getLangcode(),
       ];
+
+      if ($this->getSetting('trim_text')) {
+        if ($this->getPluginId() == 'text_summary_or_trimmed' && !empty($item->summary)) {
+          $elements[$delta]['#text'] = $item->summary;
+        }
+        else {
+          $elements[$delta]['#text'] = $item->value;
+          $render_as_summary($elements[$delta]);
+        }
+      } else {
+        $elements[$delta]['#text'] = $item->value;
+      }
     }
 
     $link = Link::fromTextAndUrl('more', $items->getEntity()->toUrl());
     $elements[] = $link->toRenderable();
 
     return $elements;
+  }
+
+  /**
+   * Pre-render callback: Renders a processed text element's #markup as a summary.
+   *
+   * @param array $element
+   *   A structured array with the following key-value pairs:
+   *   - #markup: the filtered text (as filtered by filter_pre_render_text())
+   *   - #format: containing the machine name of the filter format to be used to
+   *     filter the text. Defaults to the fallback format. See
+   *     filter_fallback_format().
+   *   - #text_summary_trim_length: the desired character length of the summary
+   *     (used by text_summary())
+   *
+   * @return array
+   *   The passed-in element with the filtered text in '#markup' trimmed.
+   *
+   * @see filter_pre_render_text()
+   * @see text_summary()
+   */
+  public static function preRenderSummary(array $element) {
+    $element['#markup'] = text_summary($element['#markup'], $element['#format'], $element['#text_summary_trim_length']);
+    return $element;
+  }
+
+  /**
+   * {@inheritdoc}
+   */
+  public static function trustedCallbacks() {
+    return ['preRenderSummary'];
   }
 
 }
