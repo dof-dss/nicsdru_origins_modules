b6d6630c9069761253b445384f56050b6dfbe6eb
omahm <hello@onemanandhismouse.com>
2020-02-07 15:35:43 +0000

    D8O-18 ~ Formatter Settings form and defaults

--- origins_common/src/Plugin/Field/FieldFormatter/HtmlListFieldFormatter.php
+++ origins_common/src/Plugin/Field/FieldFormatter/HtmlListFieldFormatter.php
@@ -4,6 +4,7 @@ namespace Drupal\origins_common\Plugin\Field\FieldFormatter;
 
 use Drupal\Core\Field\Plugin\Field\FieldFormatter\EntityReferenceFormatterBase;
 use Drupal\Core\Field\FieldItemListInterface;
+use Drupal\Core\Form\FormStateInterface;
 
 /**
  * Field formatter "HtmlListFieldFormatter".
@@ -17,6 +18,33 @@ use Drupal\Core\Field\FieldItemListInterface;
  * )
  */
 class HtmlListFieldFormatter extends EntityReferenceFormatterBase {
+
+  /**
+   * {@inheritdoc}
+   */
+  public static function defaultSettings() {
+    return [
+      'list_type' => 'ul',
+    ] + parent::defaultSettings();
+  }
+
+  /**
+   * {@inheritdoc}
+   */
+  public function settingsForm(array $form, FormStateInterface $form_state) {
+    $elements = parent::settingsForm($form, $form_state);
+    $elements['list_type'] = [
+      '#title' => $this->t('List type'),
+      '#type' => 'select',
+      '#options' => [
+        'ul' => $this->t('Unordered list'),
+        'ol' => $this->t('Ordered list'),
+      ],
+      '#default_value' => $this->getSetting('list_type'),
+    ];
+
+    return $elements;
+  }
 
   /**
    * {@inheritdoc}
