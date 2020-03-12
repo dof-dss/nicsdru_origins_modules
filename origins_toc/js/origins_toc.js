18901eec001381076cee66481af8474cb43bb10d
omahm <hello@onemanandhismouse.com>
2020-03-12 10:27:08 +0000

    D8NID-420 ~ Attach ToC ID to elements

--- origins_toc/js/origins_toc.js
+++ origins_toc/js/origins_toc.js
@@ -2,6 +2,13 @@
   Drupal.behaviors.originsToc = {
     attach: function attach(context) {
       var $context = $(context);
+      var settings = drupalSettings.origins_toc.settings;
+
+      var $toc_elements = $context.find(settings.toc_source_container + ' ' + settings.toc_element);
+
+      $toc_elements.each(function(index) {
+        $(this).attr('id', 'toc-' + index);
+      });
 
     }
   };
