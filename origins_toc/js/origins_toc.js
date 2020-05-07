3c9378404debe72f4036bf55991a7110fb4d487f
omahm <hello@onemanandhismouse.com>
2020-05-07 17:02:15 +0100

    D8NID-648 ~ Attach ToC creation even once

--- origins_toc/js/origins_toc.js
+++ origins_toc/js/origins_toc.js
@@ -3,25 +3,27 @@
   Drupal.behaviors.originsToC = {
     attach: function attach (context) {
 
-      if (typeof drupalSettings.origins_toc.settings !== 'undefined') {
-        var toc_settings = drupalSettings.origins_toc.settings;
-      } else {
-        return;
-      }
-
-      // Check if Toc is enabled for this entity type and this entity.
-      if (toc_settings.toc_enable != 1 || toc_settings.toc_entity_enable != 1) {
-        return;
-      }
-
-      // This implementation doesn't use the configuration
-      // from the toc 3rd party settings 'toc_settings'.
-      let tocHeadings = $('#main-article h2');
-      let $tocList = $('<ul class="nav-menu" />');
-      let $headingText = Drupal.t('On this page');
-      let $skipTocText = Drupal.t('Skip table of contents');
-
+      var tocHeadings = $('#main-article h2', context).once('attachToC');
       if (tocHeadings.length > 2) {
+
+        if (typeof drupalSettings.origins_toc.settings !== 'undefined') {
+          var toc_settings = drupalSettings.origins_toc.settings;
+        } else {
+          return;
+        }
+
+        // Check if Toc is enabled for this entity type and this entity.
+        if (toc_settings.toc_enable != 1 || toc_settings.toc_entity_enable != 1) {
+          return;
+        }
+
+        // This implementation doesn't use the configuration
+        // from the toc 3rd party settings 'toc_settings'.
+        let tocHeadings = $('#main-article h2');
+        let $tocList = $('<ul class="nav-menu" />');
+        let $headingText = Drupal.t('On this page');
+        let $skipTocText = Drupal.t('Skip table of contents');
+
         // Iterate each element, append an anchor id and append link to block list.
         $(tocHeadings, context).once('toc').each(function(index) {
           $(this).attr('id', 'toc-' + index);
@@ -38,6 +40,7 @@
           $tocList,
           '<a id="toc-main-skip"></a>');
       }
+
     }
   };
 })(jQuery, Drupal, drupalSettings);
