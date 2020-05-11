96b0a3bd5ad40bb226ffed77f21c3c76b639deca
omahm <hello@onemanandhismouse.com>
2020-05-11 17:24:46 +0100

    D8NID-575 ~ Build banner render array

--- origins_shamrock/modules/origins_shamrock_admin/src/Controller/ShamrockDataController.php
+++ origins_shamrock/modules/origins_shamrock_admin/src/Controller/ShamrockDataController.php
@@ -19,9 +19,20 @@ class ShamrockDataController extends ControllerBase {
   public function index() {
     $config = $this->config(ShamrockAdminForm::SETTINGS);
 
+    $build['banner'] = [
+      '#theme' => 'origins_shamrock_banner',
+      '#title' => $config->get('title'),
+      '#body' => $config->get('body'),
+      '#url' => $config->get('url'),
+    ];
+
+    $renderer = \Drupal::service('renderer');
+    $banner = $renderer->render($build);
+
     $response = new JsonResponse();
     $response->setContent(json_encode([
       'enabled' => $config->get('published'),
+      'banner' => $banner,
     ]));
 
     $response->setLastModified(new \DateTime('@' . $config->get('modified')));
