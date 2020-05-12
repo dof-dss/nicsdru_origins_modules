2af76e240eeb3f4f30d99e087e17a4e264af031b
omahm <hello@onemanandhismouse.com>
2020-05-12 07:14:59 +0100

    D8NID-575 ~ Injecting service into controller

--- origins_shamrock/modules/origins_shamrock_admin/src/Controller/ShamrockDataController.php
+++ origins_shamrock/modules/origins_shamrock_admin/src/Controller/ShamrockDataController.php
@@ -3,12 +3,38 @@
 namespace Drupal\origins_shamrock_admin\Controller;
 
 use Drupal\Core\Controller\ControllerBase;
+use Symfony\Component\DependencyInjection\ContainerInterface;
 use Symfony\Component\HttpFoundation\JsonResponse;
 use Drupal\origins_shamrock_admin\Form\ShamrockAdminForm;
+use Drupal\Core\Render\RendererInterface;
+
 /**
  * Class ShamrockDataController.
  */
 class ShamrockDataController extends ControllerBase {
+
+  /**
+   * Drupal renderer.
+   *
+   * @var Drupal\Core\Render\RendererInterface
+   */
+  protected $renderer;
+
+  /**
+   * Constructs a new ContactListingController object.
+   */
+  public function __construct(RendererInterface $renderer ) {
+    $this->renderer = $renderer;
+  }
+
+  /**
+   * {@inheritdoc}
+   */
+  public static function create(ContainerInterface $container) {
+    return new static(
+      $container->get('renderer'),
+    );
+  }
 
   /**
    * index.
@@ -26,8 +52,7 @@ class ShamrockDataController extends ControllerBase {
       '#url' => $config->get('url'),
     ];
 
-    $renderer = \Drupal::service('renderer');
-    $banner = $renderer->render($build);
+    $banner = $this->renderer->render($build);
 
     $response = new JsonResponse();
     $response->setContent(json_encode([
