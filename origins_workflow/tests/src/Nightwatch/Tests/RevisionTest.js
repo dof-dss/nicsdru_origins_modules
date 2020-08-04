2ef18161d15c0c6fa3768a3f5f9ce245c2d571fb
DuttonMa <martin@theduttons.org>
2020-08-04 15:44:09 +0100

    revision test in progress

--- origins_workflow/tests/src/Nightwatch/Tests/RevisionTest.js
+++ origins_workflow/tests/src/Nightwatch/Tests/RevisionTest.js
@@ -6,9 +6,18 @@ module.exports = {
       .drupalLogin({ name: process.env.NW_TEST_USER_PREFIX + '_admin', password: process.env.TEST_PASS });
 
     browser
-      .drupalRelativeURL('/node/13652/revisions')
+      .drupalRelativeURL('/node/4807/revisions')
       .expect.element('h1.page-title')
       .text.to.contain('Revisions for');
+
+    browser
+      .drupalRelativeURL('/node/add/article')
+      .assert.titleContains('Create Article')
+      .assert.visible('#edit-title-0-value')
+      .setValue('#edit-title-0-value', 'test title')
+      .assert.visible('#edit-field-subtheme-shs-0-0')
+      .click('#edit-field-subtheme-shs-0-0 option[value="12"]')
+      .assert.visible('#edit-submit');
 
   }
 
