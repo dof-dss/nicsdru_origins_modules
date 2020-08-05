2e5625423710d1a59ec97cde13ff0916d3ce10f9
DuttonMa <martin@theduttons.org>
2020-08-05 10:40:29 +0100

    comments

--- origins_workflow/tests/src/Nightwatch/Tests/RevisionTest.js
+++ origins_workflow/tests/src/Nightwatch/Tests/RevisionTest.js
@@ -6,18 +6,22 @@ module.exports = {
       .drupalLogin({ name: process.env.NW_TEST_USER_PREFIX + '_admin', password: process.env.TEST_PASS });
 
     browser
-      .drupalRelativeURL('/node/4807/revisions')
+      .drupalRelativeURL('/node/5087/revisions')
       .expect.element('h1.page-title')
       .text.to.contain('Revisions for');
 
+    /* Test that 'Copy to new revision' link has been inserted
+    *  by origins_workflow_preprocess_table() */
     browser
       .useXpath()
-      .expect.element('//table//td[5]//em')
-      .text.to.contain('Current revision');
-
-    browser
-      .useXpath()
-      .expect.element('//table//tr[3]//td[5]//div//div//ul//li//a')
+      .expect.element('//table//td[5]//div//div//ul//li//a')
       .text.to.contain('Copy to new revision');
+
+    /* Test that link text has been overridden with
+    * 'Create Draft of Published' by origins_workflow_preprocess_table() */
+    browser
+      .useXpath()
+      .expect.element('//table//tr[2]//td[5]//a')
+      .text.to.contain('Create Draft of Published');
   }
 };
