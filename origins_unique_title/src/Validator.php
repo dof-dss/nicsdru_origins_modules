f37da81d83a310557db5e23fb794d010c7d17c69
Johan Gant <johan.gant@gmail.com>
2020-09-11 13:49:52 +0100

    WIP: ajax callback working

---
+++ origins_unique_title/src/Validator.php
@@ -0,0 +1,15 @@
+<?php
+
+namespace Drupal\origins_unique_title;
+
+/**
+ */
+class Validator {
+
+  public function isTitleUnique(string $title, string $bundle) {
+    dump($title);
+    dump($bundle);
+    die();
+  }
+
+}
