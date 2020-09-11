394c04587b785280366f9052ea51398b6c433812
Johan Gant <johan.gant@gmail.com>
2020-09-11 15:38:17 +0100

    Add more to service class, fix render array return

--- origins_unique_title/src/UniqueTitleValidator.php
+++ origins_unique_title/src/UniqueTitleValidator.php
@@ -2,10 +2,37 @@
 
 namespace Drupal\origins_unique_title;
 
+use Drupal\Core\Entity\EntityTypeManagerInterface;
+use Symfony\Component\DependencyInjection\ContainerInterface;
+
 /**
  * Checks whether a content title is unique in a given entity bundle.
  */
 class UniqueTitleValidator {
+
+  /**
+   * The entity type manager.
+   *
+   * @var \Drupal\Core\Entity\EntityTypeManagerInterface
+   */
+  protected $entityTypeManager;
+
+  /**
+   * UniqueTitleValidator constructor.
+   * @param EntityTypeManagerInterface $entity_type_manager
+   */
+  public function __construct(EntityTypeManagerInterface $entity_type_manager) {
+    $this->entityTypeManager = $entity_type_manager;
+  }
+
+  /**
+   * {@inheritdoc}
+   */
+  public static function create(ContainerInterface $container) {
+    return new static(
+      $container->get('entity_type.manager')
+    );
+  }
 
   /**
    * Function to assess whether a node in a given bundle is unique.
@@ -18,7 +45,12 @@ class UniqueTitleValidator {
    *   Whether or not this is a unique title in this bundle.
    */
   public function isTitleUnique(string $title, string $bundle) {
-    return FALSE;
+    $result = $this->entityTypeManager->getStorage('node')->loadByProperties([
+      'type' => $bundle,
+      'title' => $title,
+    ]);
+
+    return (empty($result));
   }
 
 }
