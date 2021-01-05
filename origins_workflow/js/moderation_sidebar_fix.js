80508a68e23785730dc80a66aadff340d7d0ded0
DuttonMa <martin@theduttons.org>
2021-01-05 15:45:49 +0000

    commenting

--- origins_workflow/js/moderation_sidebar_fix.js
+++ origins_workflow/js/moderation_sidebar_fix.js
@@ -6,6 +6,12 @@
   Drupal.behaviors.landinglisting = {
     attach: (context) => {
       $("a.archive-link").click(function(event) {
+        /*
+        A class of 'archive-link' means that this link has been faked by code in
+        origins_workflow_moderation_sidebar_alter, so we complete the illusion
+        by adding a confirmation here in the same way that moderation sidebar
+        normally would.
+         */
         return confirm("When archived, this content will be unpublished. Are you sure that you want to archive?");
       });
     }
