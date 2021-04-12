55d4eff5bb9e93ae8122381a039540cc8eb50dd8
Johan Gant <johan.gant@gmail.com>
2021-04-12 11:40:23 +0100

    Move lipsum text into variable

--- origins_workflow/tests/src/Nightwatch/Tests/RevisionTest.js
+++ origins_workflow/tests/src/Nightwatch/Tests/RevisionTest.js
@@ -1,5 +1,5 @@
 module.exports = {
-  '@tags': ['origins', 'origins_workflow'],
+  '@tags': ['origins', 'origins_workflow', 'debug'],
 
   before: function (browser) {
     // Resize default window size to aid with debugging, if needed.
@@ -7,6 +7,12 @@ module.exports = {
   },
 
   'Test login': browser => {
+    const lipsumText = "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut " +
+      "labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco " +
+      "laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in " +
+      "voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat " +
+      "cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.";
+
     browser
       .drupalLogin({ name: process.env.NW_TEST_USER_PREFIX + '_admin', password: process.env.TEST_PASS });
 
@@ -18,13 +24,13 @@ module.exports = {
     browser
       .setValue('input#edit-title-0-value', [Date.now() + ' -- ' + 'RevisionTest.js test article', browser.Keys.TAB])
       .pause(2000)
-      .setValue('textarea#edit-field-summary-0-value', 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.')
+      .setValue('textarea#edit-field-summary-0-value', lipsumText)
       .waitForElementVisible('#cke_edit-body-0-value', 2000)
       .execute(function (instance, content) {
           CKEDITOR.instances[instance].setData(content);
         }, [
           'edit-body-0-value',
-          '<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>'
+          '<p> + lipsumText + </p>'
         ]
       )
       .click('input#edit-submit');
