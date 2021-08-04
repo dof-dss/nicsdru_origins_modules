9556f59b94d40599f1d8e1bc4be66ff842360119
omahm <hello@onemanandhismouse.com>
2021-08-04 20:49:38 +0100

    D8UN-627 ~ Utilities service class

---
+++ origins_translations/src/Utilities.php
@@ -0,0 +1,41 @@
+<?php
+
+namespace Drupal\origins_translations;
+
+use Drupal\Core\Config\ConfigFactoryInterface;
+
+/**
+ * Utilities service.
+ */
+class Utilities {
+
+  /**
+   * The config factory.
+   *
+   * @var \Drupal\Core\Config\ConfigFactoryInterface
+   */
+  protected $configFactory;
+
+  /**
+   * Constructs an Utilities object.
+   *
+   * @param \Drupal\Core\Config\ConfigFactoryInterface $config_factory
+   *   The config factory.
+   */
+  public function __construct(ConfigFactoryInterface $config_factory) {
+    $this->configFactory = $config_factory;
+  }
+
+  /**
+   * Returns languages from the configuration that are set to 'enabled'.
+   */
+  public function getActiveLanguages() {
+    $config = $this->configFactory->get('origins_translations.languages');
+
+    $languages = $config->getRawData();
+    unset($languages['_core']);
+
+    return array_filter($languages, static fn($language) => $language['1'] === TRUE);
+  }
+
+}
