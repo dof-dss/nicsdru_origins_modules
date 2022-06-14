ca7c1093f812a7c917ed837ab8920a3100d946e9
OMAHM <hello@onemanandhismouse.com>
2022-06-14 15:57:31 +0100

    D8NID-1517 ~ Append banner styles to the HTML document Head

--- origins_shamrock/js/shamrock_banner.js
+++ origins_shamrock/js/shamrock_banner.js
@@ -4,6 +4,7 @@
       $('body', context).once('shamrock').each(function () {
         $.getJSON(drupalSettings.origins_shamrock.service_url, function (data) {
           if (data.enabled) {
+            $('head').append(data.styling);
             $('body').prepend(data.banner_html);
           }
         });
