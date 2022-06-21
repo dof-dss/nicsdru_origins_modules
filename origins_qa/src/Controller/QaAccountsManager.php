9601127f2cbf8d517ea983f325aaf86d95241eb8
OMAHM <hello@onemanandhismouse.com>
2022-06-21 20:25:54 +0100

    D8NID-1518 ~ Basic QA accounts listing

--- origins_qa/src/Controller/QaAccountsManager.php
+++ origins_qa/src/Controller/QaAccountsManager.php
@@ -13,10 +13,35 @@ class QaAccountsManager extends ControllerBase {
    * Returns a list of QA accounts.
    */
   public function list() {
+    $build = [];
 
-    $build['content'] = [
-      '#type' => 'item',
-      '#markup' => $this->t('It works!'),
+    $accounts = $this->entityTypeManager()
+                  ->getListBuilder('user')
+                  ->getStorage()
+                  ->loadByProperties([
+                    'roles' => 'qa'
+                  ]);
+
+    $header = [
+      'username' => $this->t('Username'),
+      'status' => $this->t('Status'),
+      'last_access' => $this->t('Last access'),
+    ];
+
+    $rows = [];
+
+    foreach ($accounts as $account) {
+      $rows[] = [
+        $account->label(),
+        $account->isActive(),
+        date('d F Y', $account->getLastAccessedTime()),
+      ];
+    }
+
+    $build['qa_accounts'] = [
+      '#type' => 'table',
+      '#header' => $header,
+      '#rows' => $rows,
     ];
 
     return $build;
