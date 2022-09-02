4750388770c9c877a9fb6c4ddfcdd23a091d1e4c
DuttonMa <martin@theduttons.org>
2022-09-02 16:03:36 +0100

    coding standards

--- origins_qa/src/Commands/OriginsQACommands.php
+++ origins_qa/src/Commands/OriginsQACommands.php
@@ -6,21 +6,22 @@ use Drush\Commands\DrushCommands;
 use Drupal\origins_qa\Controller\QaAccountsManager;
 
 /**
-* Drush custom commands.
-*/
+ * Drush custom commands.
+ */
 class OriginsQaCommands extends DrushCommands {
+
   /**
-  * Drush command to enable or dsable all QA accounts.
-  *
-  * @param string $option
-  *  Argument to select 'enable' or 'disable'
-  *
-  * @command bulk_update_qa_accounts
-  */
-  public function bulk_update_qa_accounts($option = 'disable')
-  {
+   * Drush command to enable or dsable all QA accounts.
+   *
+   * @param string $option
+   *   Argument to select 'enable' or 'disable'.
+   *
+   * @command bulk_update_qa_accounts
+   */
+  public function bulkUpdateQaAccounts($option = 'disable') {
     // Use QaAccountsManager to enable or disable all QA accounts.
     $qac = new QaAccountsManager();
     $qac->toggleAll($option);
   }
+
 }
