8a6fb5e55fdb5f9ca2feae356bcd9e48ee9364c9
DuttonMa <martin@theduttons.org>
2023-07-06 15:35:27 +0100

    more sorting

--- origins_shamrock/modules/origins_shamrock_admin/src/Controller/ShamrockDataController.php
+++ origins_shamrock/modules/origins_shamrock_admin/src/Controller/ShamrockDataController.php
@@ -4,9 +4,9 @@ namespace Drupal\origins_shamrock_admin\Controller;
 
 use Drupal\Core\Controller\ControllerBase;
 use Drupal\Core\Render\RendererInterface;
+use Drupal\origins_shamrock_admin\Form\ShamrockAdminForm;
 use Symfony\Component\DependencyInjection\ContainerInterface;
 use Symfony\Component\HttpFoundation\JsonResponse;
-use Drupal\origins_shamrock_admin\Form\ShamrockAdminForm;
 
 /**
  * Provides a JSON response containing Operation Shamrock data.
