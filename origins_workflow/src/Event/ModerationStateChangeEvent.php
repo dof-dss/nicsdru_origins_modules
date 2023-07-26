be88c730f8e1a052f68f8752894a72c4e8c1b6f7
OMAHM <hello@onemanandhismouse.com>
2023-07-26 15:08:14 +0100

    DEPT-513 ~ Helper methods on moderation change event

--- origins_workflow/src/Event/ModerationStateChangeEvent.php
+++ origins_workflow/src/Event/ModerationStateChangeEvent.php
@@ -3,7 +3,11 @@
 namespace Drupal\origins_workflow\Event;
 
 use Drupal\Component\EventDispatcher\Event;
+use Drupal\Core\Entity\EntityInterface;
 
+/**
+ * Event for when an entity moderation state is changed using Origins Workflow.
+ */
 class ModerationStateChangeEvent extends Event {
 
   const CHANGE = 'moderation_state.change';
@@ -24,31 +28,53 @@ class ModerationStateChangeEvent extends Event {
 
   /**
    * @param \Drupal\Core\Entity\EntityInterface $entity
+   *   The entity for which moderation state was changed.
    * @param string $state
+   *   The requested moderation state for the entity.
    */
-  public function __construct(EntityInterface $entity, string $state)
-  {
+  public function __construct(EntityInterface $entity, string $state) {
     $this->entity = $entity;
     $this->state = $state;
   }
 
   /**
+   * Returns the entity for which the moderation state was changed.
+   *
    * @return \Drupal\Core\Entity\EntityInterface|EntityInterface
+   *   The entity which had moderation state changed..
    */
-  public function getEntity(): EntityInterface
-  {
+  public function getEntity(): EntityInterface {
     return $this->entity;
   }
 
   /**
+   * Returns the requested moderation state for the entity.
+   *
    * @return string
+   *   The moderation state.
    */
-  public function getState(): string
-  {
+  public function getState(): string {
     return $this->state;
   }
 
+  /**
+   * Returns the published status.
+   *
+   * @return bool
+   *   True if published, false if not.
+   */
+  public function isPublished(): bool {
+    return ($this->state === 'published');
+  }
 
-
+  /**
+   * Returns the archived status.
+   *
+   * @return bool
+   *   True if archived, false if not.
+   */
+  public function isArchived(): bool {
+    return ($this->state === 'archived');
+  }
 
 }
