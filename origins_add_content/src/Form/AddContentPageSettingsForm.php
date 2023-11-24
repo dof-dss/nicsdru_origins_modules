22123e7260ef472163e6f8d6f8073ca4940380aa
OMAHM <hello@onemanandhismouse.com>
2023-11-24 20:39:48 +0000

    DEPT-1535 ~ Add settings form to allow entity selection

---
+++ origins_add_content/src/Form/AddContentPageSettingsForm.php
@@ -0,0 +1,93 @@
+<?php declare(strict_types = 1);
+
+namespace Drupal\origins_add_content\Form;
+
+use Drupal\Core\Entity\ContentEntityTypeInterface;
+use Drupal\Core\Entity\EntityTypeManagerInterface;
+use Drupal\Core\Form\ConfigFormBase;
+use Drupal\Core\Form\FormStateInterface;
+use Symfony\Component\DependencyInjection\ContainerInterface;
+
+/**
+ * Configure Origins Add content settings.
+ */
+final class AddContentPageSettingsForm extends ConfigFormBase {
+
+  /**
+   * The entity type manager.
+   *
+   * @var \Drupal\Core\Entity\EntityTypeManagerInterface
+   */
+  protected $entityTypeManager;
+
+  /**
+   * Class constructor.
+   */
+  public function __construct(EntityTypeManagerInterface $entity_type_manager) {
+    $this->entityTypeManager = $entity_type_manager;
+  }
+
+  /**
+   * {@inheritdoc}
+   */
+  public static function create(ContainerInterface $container) {
+    return new static(
+      $container->get('entity_type.manager'),
+    );
+  }
+
+  /**
+   * {@inheritdoc}
+   */
+  public function getFormId(): string {
+    return 'origins_add_content_add_content_page_settings';
+  }
+
+  /**
+   * {@inheritdoc}
+   */
+  protected function getEditableConfigNames(): array {
+    return ['origins_add_content.settings'];
+  }
+
+  /**
+   * {@inheritdoc}
+   */
+  public function buildForm(array $form, FormStateInterface $form_state): array {
+    $config = $this->config('origins_add_content.settings');
+    $entities = $this->entityTypeManager->getDefinitions();
+    $options = [];
+
+    foreach ($entities as $entity_type) {
+
+      if ($entity_type instanceof ContentEntityTypeInterface) {
+        $options[$entity_type->id()] = $entity_type->getLabel();
+      }
+    }
+
+    $form['add_content_page_entities'] = [
+      '#type' => 'checkboxes',
+      '#title' => $this->t('Content Entity types links'),
+      '#options' => $options,
+      '#default_value' => $config->get('add_content_page_entities') ?: [],
+      '#description' => $this->t('Selected entities will have a link added to the Add content (node/add) admin page.')
+    ];
+
+    return parent::buildForm($form, $form_state);
+  }
+
+  /**
+   * {@inheritdoc}
+   */
+  public function submitForm(array &$form, FormStateInterface $form_state): void {
+    $values = $form_state->getValues();
+    $entity_types = array_keys(array_filter($values['add_content_page_entities']));
+
+    $this->config('origins_add_content.settings')
+      ->set('add_content_page_entities', $entity_types)
+      ->save();
+
+    parent::submitForm($form, $form_state);
+  }
+
+}
