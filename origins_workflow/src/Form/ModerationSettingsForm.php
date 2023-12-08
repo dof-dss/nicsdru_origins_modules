8a518d64dbc5152aa86f46e9244e2a1115a8004b
OMAHM <hello@onemanandhismouse.com>
2023-12-08 03:57:07 +0000

    DEPT-632 ~ WiP - Config form listing all Workflow displays

---
+++ origins_workflow/src/Form/ModerationSettingsForm.php
@@ -0,0 +1,60 @@
+<?php declare(strict_types = 1);
+
+namespace Drupal\origins_workflow\Form;
+
+use Drupal\Core\Form\ConfigFormBase;
+use Drupal\Core\Form\FormStateInterface;
+use Drupal\views\Entity\View;
+use Drupal\views\Views;
+
+/**
+ * Configure Origins: Moderation settings for this site.
+ */
+final class ModerationSettingsForm extends ConfigFormBase {
+
+  /**
+   * {@inheritdoc}
+   */
+  public function getFormId(): string {
+    return 'origins_workflow_moderation_settings';
+  }
+
+  /**
+   * {@inheritdoc}
+   */
+  protected function getEditableConfigNames(): array {
+    return ['origins_workflow.settings'];
+  }
+
+  /**
+   * {@inheritdoc}
+   */
+  public function buildForm(array $form, FormStateInterface $form_state): array {
+
+    $view = Views::getView('workflow_moderation');
+    $displays = $view->storage->get('display');
+    unset($displays['default']);
+
+    foreach ($displays as $display => $data) {
+      ksm($data);
+      $form[$display] = array(
+        '#type' => 'details',
+        '#title' => $data['display_title'],
+      );
+    }
+
+    return parent::buildForm($form, $form_state);
+  }
+
+
+  /**
+   * {@inheritdoc}
+   */
+  public function submitForm(array &$form, FormStateInterface $form_state): void {
+    $this->config('origins_workflow.settings')
+      ->set('example', $form_state->getValue('example'))
+      ->save();
+    parent::submitForm($form, $form_state);
+  }
+
+}
