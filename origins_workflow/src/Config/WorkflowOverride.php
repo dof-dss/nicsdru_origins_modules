50b8440684a828d062422da374451984bf5ab7d5
OMAHM <hello@onemanandhismouse.com>
2023-12-12 16:06:37 +0000

    DEPT-632 ~ Config override to alter display content type filter

---
+++ origins_workflow/src/Config/WorkflowOverride.php
@@ -0,0 +1,54 @@
+<?php
+
+namespace Drupal\origins_workflow\Config;
+
+use Drupal\Core\Cache\CacheableMetadata;
+use Drupal\Core\Config\ConfigFactoryOverrideInterface;
+use Drupal\Core\Config\StorageInterface;
+use Drupal\origins_workflow\Form\ModerationSettingsForm;
+
+class WorkflowOverride implements ConfigFactoryOverrideInterface {
+
+  /**
+   * {@inheritdoc}
+   */
+  public function loadOverrides($names) {
+    $overrides = [];
+    if (in_array('views.view.workflow_moderation', $names)) {
+
+      $config = \Drupal::configFactory()->get(ModerationSettingsForm::SETTINGS)->getRawData();
+
+      foreach ($config['view_overrides'] as $display => $data) {
+
+        $filtered_content_types = array_filter($data['filtered_node_types'] ?? [], 'is_string');
+
+        if (!empty($filtered_content_types)) {
+          $overrides['views.view.workflow_moderation']['display'][$display]['display_options']['filters']['type']['value'] = $filtered_content_types;
+          $overrides['views.view.workflow_moderation']['display']['all_drafts']['display_options']['filters']['type']['expose']['reduce'] = true;
+        }
+      }
+    }
+    return $overrides;
+  }
+
+  /**
+   * {@inheritdoc}
+   */
+  public function getCacheSuffix() {
+    return 'OriginsWorkflowConfigOverrider';
+  }
+
+  /**
+   * {@inheritdoc}
+   */
+  public function getCacheableMetadata($name) {
+    return new CacheableMetadata();
+  }
+
+  /**
+   * {@inheritdoc}
+   */
+  public function createConfigObject($name, $collection = StorageInterface::DEFAULT_COLLECTION) {
+    return NULL;
+  }
+}
