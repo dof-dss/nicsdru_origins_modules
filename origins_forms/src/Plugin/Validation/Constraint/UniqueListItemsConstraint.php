1d3d83e2ac915e9dfde402c5c218dee0ebae99aa
omahm <hello@onemanandhismouse.com>
2025-03-05 20:11:10 +0000

    DEPT-1161 ~ Field validator for unique entity reference lists

---
+++ origins_forms/src/Plugin/Validation/Constraint/UniqueListItemsConstraint.php
@@ -0,0 +1,22 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Drupal\origins_forms\Plugin\Validation\Constraint;
+
+use Symfony\Component\Validator\Constraint;
+
+/**
+ * Provides a constraint that prevents duplicate entries in fields that
+ * implement the FieldItemList interface.
+ *
+ * @Constraint(
+ *   id = "unique_list_items",
+ *   label = @Translation("Unique List Items", context = "Validation"),
+ * )
+ */
+final class UniqueListItemsConstraint extends Constraint {
+
+  public string $message = 'Duplicate items detected. The list must contain only unique entries.';
+
+}
