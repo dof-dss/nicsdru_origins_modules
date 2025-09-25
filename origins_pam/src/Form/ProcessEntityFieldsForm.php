27c7672b03dbbb0be85f3e301d7f20999bd2f84c
omahm <hello@onemanandhismouse.com>
2025-09-25 10:20:56 +0100

    D8NID-1822 ~ WiP: Build array of entity field metadata and lookup fields with internal links

--- origins_pam/src/Form/ProcessEntityFieldsForm.php
+++ origins_pam/src/Form/ProcessEntityFieldsForm.php
@@ -4,8 +4,10 @@ declare(strict_types=1);
 
 namespace Drupal\origins_pam\Form;
 
+use Drupal\Core\Batch\BatchBuilder;
 use Drupal\Core\Form\FormBase;
 use Drupal\Core\Form\FormStateInterface;
+use Drupal\Core\Url;
 use Drupal\field\Entity\FieldStorageConfig;
 
 /**
@@ -95,8 +97,142 @@ final class ProcessEntityFieldsForm extends FormBase {
       $fields_data[$entity_type][$field] = $tables;
     }
 
-    ksm($fields_data);
+    $db = \Drupal::database();
 
+    $batch = new BatchBuilder();
+    $batch->setTitle('Updating URL aliases')
+      ->setFinishCallback([self::class, 'batchFinished'])
+      ->setInitMessage('Starting field alias processing')
+      ->setProgressMessage('Processing...')
+      ->setErrorMessage('An error occurred during processing.');
+
+    foreach ($fields_data as $entity_type => $fields) {
+      foreach ($fields as $field => $tables) {
+        foreach ($tables as $table) {
+
+          $field_name = substr($field, strrpos($field, '.') + 1);
+
+          $entity_ids = $db->select($table, 't')
+            ->fields('t', ['entity_id'])
+            ->condition('t.' . $field_name . '_value', '%href="/%', 'LIKE', )
+            ->distinct()->execute()->fetchCol();
+
+          $entity_id_chunks = array_chunk($entity_ids, 250);
+
+          foreach ($entity_id_chunks as $chunk_id => $entity_ids) {
+            $args = [
+              $chunk_id,
+              $entity_ids,
+              $table,
+              $field_name,
+            ];
+            $batch->addOperation([self::class, 'batchProcess'], $args);
+          }
+        }
+      }
+    }
+
+    batch_set($batch->toArray());
+
+    $form_state->setRedirectUrl(Url::fromRoute('origins_pam.process_form'));
+  }
+
+  public static function fetchRelativeUrls($html) {
+    $pattern = '/<a\b[^>]*\shref\s*=\s*["\']([^"\']*)["\'][^>]*>/i';
+    preg_match_all($pattern, $html, $matches);
+
+    $urls = [];
+    foreach ($matches[1] as $href) {
+      if (strpos($href, '/') === 0) {
+        $urls[] = $href;
+      }
+    }
+
+    return $urls;
+  }
+
+  public static function batchProcess(int $chunk_id, array $entity_ids, string $table, string $field_name, array &$context): void {
+    if (!isset($context['sandbox']['progress'])) {
+      $context['sandbox']['progress'] = 0;
+      $context['sandbox']['max'] = 1000;
+    }
+    if (!isset($context['results']['updated'])) {
+      $context['results']['updated'] = 0;
+      $context['results']['skipped'] = 0;
+      $context['results']['failed'] = 0;
+      $context['results']['progress'] = 0;
+      $context['results']['process'] = 'Form batch completed';
+    }
+
+    $context['results']['progress'] += count($entity_ids);
+
+    $context['message'] = t('Processing batch #@batch_id batch size @batch_size for total @count items.', [
+      '@batch_id' => number_format($chunk_id),
+      '@batch_size' => number_format(count($entity_ids)),
+      '@count' => number_format($context['sandbox']['max']),
+    ]);
+
+    $db = \Drupal::database();
+
+    foreach ($entity_ids as $entity_id) {
+      $value_field = $field_name . "_value";
+      $field_results = $db->select($table, 't')
+        ->fields('t', ['revision_id', $value_field])
+        ->condition('t.entity_id', $entity_id)
+        ->execute()->fetchAllKeyed(0);
+
+      foreach ($field_results as $revision_id => $field_value) {
+        $url_matches = self::fetchRelativeUrls($field_value);
+
+        foreach ($url_matches as $url_alias) {
+          if (!str_starts_with($url_alias, '/node')) {
+            $canonical_url = $db->select('path_alias', 'pa')
+              ->fields('pa', ['path'])
+              ->condition('alias', $url_alias)
+              ->execute()->fetchCol();
+
+            if ($canonical_url) {
+              $db->query("UPDATE $table SET $value_field = REPLACE(url, $url_alias, $canonical_url)")->execute();
+            }
+
+          }
+        }
+      }
+    }
+  }
+
+  public static function batchFinished(bool $success, array $results, array $operations, string $elapsed): void {
+    $messenger = \Drupal::messenger();
+
+    if ($success) {
+      $messenger->addMessage(t('@process processed @count, skipped @skipped, updated @updated, failed @failed in @elapsed.', [
+        '@process' => $results['process'],
+        '@count' => $results['progress'],
+        '@skipped' => $results['skipped'],
+        '@updated' => $results['updated'],
+        '@failed' => $results['failed'],
+        '@elapsed' => $elapsed,
+      ]));
+      \Drupal::logger('batch_form_example')->info(
+        '@process processed @count, skipped @skipped, updated @updated, failed @failed in @elapsed.', [
+        '@process' => $results['process'],
+        '@count' => $results['progress'],
+        '@skipped' => $results['skipped'],
+        '@updated' => $results['updated'],
+        '@failed' => $results['failed'],
+        '@elapsed' => $elapsed,
+      ]);
+    }
+    else {
+      $error_operation = reset($operations);
+      if ($error_operation) {
+        $message = t('An error occurred while processing %error_operation with arguments: @arguments', [
+          '%error_operation' => print_r($error_operation[0]),
+          '@arguments' => print_r($error_operation[1], TRUE),
+        ]);
+        $messenger->addError($message);
+      }
+    }
   }
 
 }
