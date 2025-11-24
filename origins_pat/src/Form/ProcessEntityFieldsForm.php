4ac4b628a4ac4ee21e09324889ca5210de34ca5c
omahm <hello@onemanandhismouse.com>
2025-11-24 13:21:51 +0000

    D8NID-1822 ~ Fix self referencing links with anchor and generate self-ref report

--- origins_pat/src/Form/ProcessEntityFieldsForm.php
+++ origins_pat/src/Form/ProcessEntityFieldsForm.php
@@ -523,13 +523,33 @@ final class ProcessEntityFieldsForm extends FormBase {
         $url_entity = $entity_type_manager->getStorage($url_entity_type)->load($url_entity_id);
         $is_self_referencing = $url_entity->id() === $linking_entity->id();
 
-        if ($is_self_referencing && str_contains($link_url, '#')) {
-          // @phpstan-ignore-next-line.
+        if ($is_self_referencing) {
+          if (str_contains($link_url, '#')) {
+            $link_element->setAttribute('href', substr($link_url, strrpos($link_url, '#')));
+            $field_is_updated = TRUE;
+            $context['results']['updated'] = $context['results']['updated'] + 1;
 
-          $link_element->setAttribute('href', substr($link_url, strrpos($link_url, '#')));
-          $field_is_updated = TRUE;
-          $context['results']['updated'] = $context['results']['updated'] + 1;
-        } else {
+            $context['report']['selfref'][] = [
+              $linking_entity_url,
+              $link_element->getAttribute('href'),
+              '### FIXED ###',
+              '### AUTOMATICALLY UPDATED ANCHOR LINK ###',
+              $domain,
+              $moderation_status
+            ];
+          }
+          else {
+            $context['report']['selfref'][] = [
+              $linking_entity_url,
+              $link_element->textContent,
+              $link_element->previousSibling?->textContent,
+              $link_element->nextSibling?->textContent,
+              $domain,
+              $moderation_status
+            ];
+          }
+        }
+        else {
           // @phpstan-ignore-next-line.
           $link_element->setAttribute('href', $internal_url->getInternalPath());
           // @phpstan-ignore-next-line.
@@ -540,35 +560,24 @@ final class ProcessEntityFieldsForm extends FormBase {
           $link_element->setAttribute('data-entity-substitution', 'canonical');
           $field_is_updated = TRUE;
           $context['results']['updated'] = $context['results']['updated'] + 1;
-        }
 
-        if (!$is_self_referencing && $context['report']['size'] > 0 && (rand(1, 100) <= ($context['report']['size'] * 10))) {
-          $link_entity_moderation_status = 'unknown';
-          if ($url_entity->hasField('moderation_state')) {
-            $link_entity_moderation_status = $url_entity->get('moderation_state')->getString();
+          if ($context['report']['size'] > 0 && (rand(1, 100) <= ($context['report']['size'] * 10))) {
+            $link_entity_moderation_status = 'unknown';
+            if ($url_entity->hasField('moderation_state')) {
+              $link_entity_moderation_status = $url_entity->get('moderation_state')->getString();
+            }
+
+            $context['report']['links'][] = [
+              $linking_entity_url,
+              $moderation_status,
+              $url_entity->uuid(),
+              $url_entity->label(),
+              $link_url,
+              $link_entity_moderation_status,
+              ($is_redirected) ? 'Yes' : 'No',
+              $domain,
+            ];
           }
-
-          $context['report']['links'][] = [
-            $linking_entity_url,
-            $moderation_status,
-            $url_entity->uuid(),
-            $url_entity->label(),
-            $link_url,
-            $link_entity_moderation_status,
-            ($is_redirected) ? 'Yes' : 'No',
-            $domain,
-          ];
-        }
-        elseif ($is_self_referencing) {
-
-          $context['report']['selfref'][] = [
-            $linking_entity_url,
-            $link_element->textContent,
-            $link_element->previousSibling?->textContent,
-            $link_element->nextSibling?->textContent,
-            $domain,
-            $moderation_status
-          ];
         }
       }
       else {
@@ -618,6 +627,16 @@ final class ProcessEntityFieldsForm extends FormBase {
 
       fclose($report_file);
     }
+    if (!empty($context['report']['selfref'])) {
+      $report_file = fopen(self::SELFREF_FILENAME, 'a');
+
+      foreach ($context['report']['selfref'] as $report_entry) {
+        fputcsv($report_file, $report_entry, ',', '"', '');
+      }
+
+      fclose($report_file);
+    }
+
   }
 
   /**
