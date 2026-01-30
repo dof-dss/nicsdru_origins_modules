083021d3dcffd1d344d504e080d17934f5db1971
omahm <hello@onemanandhismouse.com>
2026-01-30 14:59:31 +0000

    DEPT-1304 ~ Caution and lockdown node feature for revisions

--- origins_forms/src/Form/FormsConfigurationForm.php
+++ origins_forms/src/Form/FormsConfigurationForm.php
@@ -30,12 +30,62 @@ final class FormsConfigurationForm extends ConfigFormBase {
    * {@inheritdoc}
    */
   public function buildForm(array $form, FormStateInterface $form_state): array {
+
+    $max_revisions_warning_caution_limit = 300;
+
     $form['form_descriptions'] = [
       '#type' => 'checkbox',
       '#title' => $this->t('Enable form descriptions'),
       '#description' => $this->t('Moves the description on content form fields underneath the title.'),
       '#default_value' => $this->config('origins_forms.settings')->get('enable_form_descriptions'),
     ];
+
+    $form['revisions_warning'] = [
+      '#type' => 'checkbox',
+      '#title' => $this->t('Revisions warning'),
+      '#description' => $this->t('Warn the user when editing a node that has excessive revisions.'),
+      '#default_value' => $this->config('origins_forms.settings')->get('enable_revisions_warning'),
+    ];
+
+    $form['revisions_warning_settings'] = [
+      '#type' => 'details',
+      '#open' => TRUE,
+      '#title' => $this->t('Revisions warning settings'),
+      '#states' => [
+        'visible' => [
+          ':input[name="revisions_warning"]' => ['checked' => TRUE],
+        ],
+      ],
+    ];
+
+    $form['revisions_warning_settings']['revisions_warning_caution_limit'] = [
+      '#type' => 'number',
+      '#min' => 10,
+      '#max' => $max_revisions_warning_caution_limit,
+      '#title' => $this->t('Caution limit'),
+      '#description' => $this->t('The number of revisions after which a caution will be displayed to the user.'),
+      '#default_value' => $this->config('origins_forms.settings')->get('revisions_warning_caution_limit'),
+      '#states' => [
+        'invisible' => [
+          ':input[name="revisions_warning"]' => ['checked' => FALSE],
+        ],
+      ],
+    ];
+
+    $form['revisions_warning_settings']['revisions_warning_lockdown_limit'] = [
+      '#type' => 'number',
+      '#min' => $max_revisions_warning_caution_limit,
+      '#max' => $max_revisions_warning_caution_limit + $max_revisions_warning_caution_limit * 2,
+      '#title' => $this->t('Lockdown limit'),
+      '#description' => $this->t('The number of revisions after which the node will be locked from editing.'),
+      '#default_value' => $this->config('origins_forms.settings')->get('revisions_warning_lockdown_limit'),
+      '#states' => [
+        'invisible' => [
+          ':input[name="revisions_warning"]' => ['checked' => FALSE],
+        ],
+      ],
+    ];
+
     return parent::buildForm($form, $form_state);
   }
 
@@ -45,6 +95,9 @@ final class FormsConfigurationForm extends ConfigFormBase {
   public function submitForm(array &$form, FormStateInterface $form_state): void {
     $this->config('origins_forms.settings')
       ->set('enable_form_descriptions', $form_state->getValue('form_descriptions'))
+      ->set('enable_revisions_warning', $form_state->getValue('revisions_warning'))
+      ->set('revisions_warning_caution_limit', $form_state->getValue('revisions_warning_caution_limit'))
+      ->set('revisions_warning_lockdown_limit', $form_state->getValue('revisions_warning_lockdown_limit'))
       ->save();
     parent::submitForm($form, $form_state);
   }
