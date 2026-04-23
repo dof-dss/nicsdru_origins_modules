95be74d55a6e050a878dfd0f7208a14db535f847
omahm <hello@onemanandhismouse.com>
2026-04-23 09:47:06 +0100

    D8O-51 ~ Display Origins tours in help section

--- origins_tour/src/Controller/OriginsTourController.php
+++ origins_tour/src/Controller/OriginsTourController.php
@@ -15,8 +15,29 @@ final class OriginsTourController extends ControllerBase {
    * Returns the help homepage render array.
    */
   public function __invoke(): array {
+    $tour_links = [];
+
+    $tours = $this->entityTypeManager()->getStorage('tour')->loadMultiple();
+    foreach ($tours as $tour) {
+
+      // We only want to display Origins tours in our help section.
+      if (substr($tour->id(), 0, 8) === 'origins_') {
+        $tour_links[] = [
+          '#type' => 'link',
+          '#title' => $tour->label(),
+          '#url' => $tour->toUrl(),
+        ];
+      }
+    }
+
     return [
-      '#theme' => 'help_homepage',
+      'homepage' => [
+        '#theme' => 'help_homepage',
+      ],
+      'tours' => [
+        '#theme' => 'tours',
+        '#tours' => $tour_links
+      ],
     ];
   }
 
